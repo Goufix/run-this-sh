64b6b8b00ff1a5b7bd1fa29a89c64c2ae795924d
Alifer Quevedo <alifer.17.11@gmail.com>
2023-04-25 10:57:54 -0300

    :rocket

---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let runScript = vscode.commands.registerCommand("run-this-shell.run", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    let currentFilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+    currentFilePath = currentFilePath?.split("\\").join("/");
+    console.log("currentFilePath", currentFilePath);
+
+    if (!currentFilePath) {
+      return;
+    }
+    if (vscode.window.activeTextEditor?.document.languageId === "shellscript") {
+      const terminal =
+        vscode.window.terminals.length > 0
+          ? vscode.window.terminals[0]
+          : vscode.window.createTerminal();
+      terminal.show();
+      terminal.sendText(`bash ${currentFilePath}`);
+    }
+  });
+
+  context.subscriptions.push(runScript);
+
+  const button = loadButton();
+
+  context.subscriptions.push(button);
+}
+
+function loadButton() {
+  const runButton = vscode.window.createStatusBarItem(
+    "run-this-shell.run", // id
+    vscode.StatusBarAlignment.Left
+  );
+  runButton.text = "Run this shell script";
+  runButton.command = "run-this-shell.run";
+  runButton.show();
+
+  return runButton;
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
